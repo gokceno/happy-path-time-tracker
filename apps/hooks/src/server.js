19ed39ba927a3c1ad9d320c87dae516c1c5adc0c
Gokcen Ogutcu <gokcen@brewww.com>
2023-05-02 07:44:18 +0300

    fix: added a new way to find the timerId, based on hook op type

--- apps/hooks/src/server.js
+++ apps/hooks/src/server.js
@@ -38,55 +38,61 @@ const GraphQLClient = new Client({
 });
 
 app.post('/timers/update/total-duration', async function (req, res, next) {
-  let timerId = 0;
-  console.log(req.body.keys);
-  if(Array.isArray(req.body.keys) && req.body.keys.length > 0) {
-    timerId = req.body.keys[0];
-  }
+  // Find timerId
+  let timerId = undefined;
+  if(req.body.event == 'timers.items.create') { timerId = req.body.key; }
+  else if(req.body.event == 'timers.items.update') { timerId = req.body.keys[0]; }
   else {
-    timerId = req.body.keys;
+    res.log.debug(req.body);
+    res.status(403).send({error: `Requested hook type doesn't exist. Exiting.`});
   }
-  const TimersQuery = `
-  query timers_by_id($timerId: ID!) {
-    timers_by_id(id: $timerId) {
-      id
-      starts_at
-      ends_at
-      duration
-      total_duration
-    }
-  }
-  `;
-  const queryResponse = await GraphQLClient.query(TimersQuery, { timerId });
-  if(queryResponse.data != undefined && queryResponse.data.timers_by_id != undefined) {
-    // Calculate totalDuration
-    const startsAt = DateTime.fromISO(queryResponse.data.timers_by_id.starts_at);
-    const endsAt = DateTime.fromISO(queryResponse.data.timers_by_id.ends_at);
-    const duration = endsAt.diff(startsAt, 'minutes');
-    const { minutes: durationInMinutes } = duration.toObject();
-    const totalDuration = Math.ceil(durationInMinutes + queryResponse.data.timers_by_id.duration);
-      // Update totalDuration
-    if(queryResponse.data.timers_by_id.total_duration !== totalDuration) {
-      const TimersMutation = `
-      mutation ppdate_timers_item($timerId: ID!, $totalDuration: Int!) {
-        update_timers_item(id: $timerId, data: {total_duration: $totalDuration}) {
-          id
-          total_duration
-        }
+  if(timerId != undefined) {
+    const TimersQuery = `
+    query timers_by_id($timerId: ID!) {
+      timers_by_id(id: $timerId) {
+        id
+        starts_at
+        ends_at
+        duration
+        total_duration
       }
-      `;
-      const mutationResponse = await GraphQLClient.mutation(TimersMutation, { timerId, totalDuration });
+    }
+    `;
+    const queryResponse = await GraphQLClient.query(TimersQuery, { timerId });
+    if(queryResponse.data != undefined && queryResponse.data.timers_by_id != undefined) {
+      // Calculate totalDuration
+      const startsAt = DateTime.fromISO(queryResponse.data.timers_by_id.starts_at);
+      const endsAt = DateTime.fromISO(queryResponse.data.timers_by_id.ends_at);
+      const duration = endsAt.diff(startsAt, 'minutes');
+      const { minutes: durationInMinutes } = duration.toObject();
+      const totalDuration = Math.ceil(durationInMinutes + queryResponse.data.timers_by_id.duration);
+      // Update totalDuration
+      if(queryResponse.data.timers_by_id.total_duration !== totalDuration) {
+        const TimersMutation = `
+        mutation ppdate_timers_item($timerId: ID!, $totalDuration: Int!) {
+          update_timers_item(id: $timerId, data: {total_duration: $totalDuration}) {
+            id
+            total_duration
+          }
+        }
+        `;
+        const mutationResponse = await GraphQLClient.mutation(TimersMutation, { timerId, totalDuration });
       // Return payload
-      res.json({ok: true, data: mutationResponse.data.update_timers_item});
+        res.json({ok: true, data: mutationResponse.data.update_timers_item});
+      }
+      else {
+        res.log.info(`Timer id: ${queryResponse.data.timers_by_id.id} total duration already updated.`);
+        res.status(403).send({error: `Timer id: ${queryResponse.data.timers_by_id.id} total duration already updated.`});
+      }
     }
     else {
-      res.log.info(`Timer id: ${queryResponse.data.timers_by_id.id} total duration already updated.`);
-      res.status(403).send({error: `Timer id: ${queryResponse.data.timers_by_id.id} total duration already updated.`});
+      res.log.info(`No timer entry was found for timerId ${timerId}`);
+      res.status(404).send({error: `No timer entry was found for timerId ${timerId}`});
     }
   }
   else {
-    res.log.info(`No timer entry was found for timerId ${timerId}`);
-    res.status(404).send({error: `No timer entry was found for timerId ${timerId}`});
+    res.log.debug(req.body);
+    res.status(412).send({error: `timerId is not present. Exiting.`});
   }
 });
 
