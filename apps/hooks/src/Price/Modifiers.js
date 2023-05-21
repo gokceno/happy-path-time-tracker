04de3cf317f7d6614917cd87046fbc873fdee655
Gokcen Ogutcu <gokcen@brewww.com>
2023-05-21 06:52:02 +0300

    feat: added "overtime" price modifier

--- apps/hooks/src/Price/Modifiers.js
+++ apps/hooks/src/Price/Modifiers.js
@@ -12,4 +12,17 @@ const noLessThanOneHour = (price, totalDuration, startsAt, endsAt)  => {
 	return (totalDuration < 60) ? (price * (60 / totalDuration)) : (price * 1);
 }
 
-module.exports = { weekends, noLessThanOneHour }
+const overtime = (price, totalDuration, startsAt, endsAt) => {
+	const weekdays = [1, 2, 3, 4, 5];
+	const overtimeHours = [19, 20, 21, 22, 23, 0, 1, 2, 3, 4, 5, 6, 7];
+	if(!(startsAt.TIME_24_WITH_SECONDS == '00:00:00' && endsAt.TIME_24_WITH_SECONDS == '00:00:00')) {
+		if(!(weekdays.includes(startsAt.weekday) || weekdays.includes(endsAt.weekday))) {
+			if(overtimeHours.includes(startsAt.hour) || overtimeHours.includes(endsAt.hour)) {
+				return price * 1.5;
+			}
+		}
+	}
+	return price * 1;
+}
+
+module.exports = { weekends, noLessThanOneHour, overtime }
