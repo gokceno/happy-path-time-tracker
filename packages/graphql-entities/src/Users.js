07a75b19649222148bd647a7fdcd8367c0c0f81b
Gokcen Ogutcu <gokcen@brewww.com>
2023-07-06 09:16:53 +0300

    feat: added users.findUserId

--- packages/graphql-entities/src/Users.js
+++ packages/graphql-entities/src/Users.js
@@ -24,10 +24,64 @@ const Users = ({ graphqlClient }) => {
       logger.error(response.error);
     }
   }
+  const findUserId = async (params) => {
+    const { externalUserId, email, did, userId } = params;
+    if(userId == undefined && externalUserId == undefined && email == undefined && did == undefined) throw new Error('A user identifier must be set');
+    if(userId !== undefined) return +userId;
+    if(did !== undefined) {
+      const UserQuery = `
+      query users($did: String!) {
+        users(filter: {did: {_eq: $did}}) {
+          id
+        }
+      }
+      `;
+      const queryResponse = await graphqlClient.query(UserQuery, { did });
+      if(queryResponse.data != undefined && typeof queryResponse.data.users == 'object') {
+        return +queryResponse.data.users[0].id;
+      }
+      else {
+        throw new Error(queryResponse.error);
+      }
+    }
+    if(email !== undefined) {
+      const UserQuery = `
+      query users($email: String!) {
+        users(filter: {email: {_eq: $email}}) {
+          id
+        }
+      }
+      `;
+      const queryResponse = await graphqlClient.query(UserQuery, { email });
+      if(queryResponse.data != undefined && typeof queryResponse.data.users == 'object') {
+        return +queryResponse.data.users[0].id;
+      }
+      else {
+        throw new Error(queryResponse.error);
+      }
+    }
+    if(externalUserId !== undefined) {
+      const UserQuery = `
+      query users($externalUserId: String!) {
+        users(filter: {slack_user_id: {_eq: $externalUserId}}) {
+          id
+        }
+      }
+      `;
+      const queryResponse = await graphqlClient.query(UserQuery, { externalUserId });
+      if(queryResponse.data != undefined && typeof queryResponse.data.users == 'object') {
+        return +queryResponse.data.users[0].id;
+      }
+      else {
+        throw new Error(queryResponse.error);
+      }
+    }
+    return null;
+  }
   const sync = async(params) => {
     return await _create(params);
   }
-  return { sync }
+  return { sync, findUserId }
 }
 
 export { Users }
