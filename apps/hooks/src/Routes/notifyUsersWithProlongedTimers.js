c9a0ca564b1d687699a23143a3f600a4f8efa568
Gokcen Ogutcu <gokcen@brewww.com>
2023-07-09 13:55:01 +0300

    fix: prolonged timers not able to get slack_id

--- apps/hooks/src/Routes/notifyUsersWithProlongedTimers.js
+++ apps/hooks/src/Routes/notifyUsersWithProlongedTimers.js
@@ -10,72 +10,83 @@ Array.prototype.random = function () {
 }
 
 const notifyUsersWithProlongedTimers = async (req, res, next) => {
-  // TODO: Email users may not have slack ids, in that case messages  wouldn'e be delivered
-  // FIXME: Slack messages are not delivered
-  if(req.body.data != undefined && typeof req.body.data == 'object') {
-    req.body.data.forEach(async (item) => {
+  const startAt = DateTime.now().minus({ minutes: process.env.PROLONGED_TIMER_TRESHOLD_1 || 240 }).toISO();
+  const queryResponse = await GraphQLClient.query(TimersWithNoEndDateQuery,{ startAt });
+  if(queryResponse?.data?.timers != undefined && queryResponse?.data?.timers.length > 0) {
+    queryResponse.data.timers.forEach(async (item) => {
       res.log.debug(item);
-      if(item.ends_at == undefined || item.ends_at == null) {
-        const startsAt = DateTime.fromISO(item.starts_at);
-        const { minutes: durationInMinutes } = DateTime.now().diff(startsAt, 'minutes').toObject();
-        if(durationInMinutes >= (process.env.PROLONGED_TIMER_SHUTDOWN_TRESHOLD || 480)) {
-          const mutation = await GraphQLClient.mutation(StopTimerMutation, { 
-            timerId: item.id, 
-            endsAt: DateTime.now().toISO() 
-          });
-          if(mutation.error == undefined) {
-            try {
-              await slackClientApp.client.chat.postMessage({
-                channel: item.user_id,
-                text: `Ok, let's stop your timer now 🏁, you've done great. Go ahead and start a new timer ⏱️ if you're still here.`
-              });
-            }
-            catch(e) {
-              res.log.error(e);
-            }
-          }
-          else {
-            res.log.error(mutation.error);
-          }
-        }
-        else if(durationInMinutes >= (process.env.PROLONGED_TIMER_TRESHOLD_2 || 420)) {
+      const startsAt = DateTime.fromISO(item.starts_at);
+      const { minutes: durationInMinutes } = DateTime.now().diff(startsAt, 'minutes').toObject();
+      if(durationInMinutes >= (process.env.PROLONGED_TIMER_SHUTDOWN_TRESHOLD || 480)) {
+        const mutation = await GraphQLClient.mutation(StopTimerMutation, { 
+          timerId: item.id, 
+          endsAt: DateTime.now().toISO() 
+        });
+        if(mutation.error == undefined) {
           try {
             await slackClientApp.client.chat.postMessage({
-              channel: item.user_id,
-              text: `You're marvellous 👑, but I start to worry. Are you still there? 👀 I'll shut down your timer within the next hour.`
+              channel: item.user_id.slack_user_id,
+              text: `Ok, let's stop your timer now 🏁, you've done great. Go ahead and start a new timer ⏱️ if you're still here.`
             });
           }
           catch(e) {
             res.log.error(e);
           }
         }
-        else if(durationInMinutes >= (process.env.PROLONGED_TIMER_TRESHOLD_1 || 240)) {
-          const messages = [
-            `You're such a hard worker 💪. It's been hours ⏱️ and you're still going. Keep it up. 👍`,
-            `It's been quite some time since you started 👀, care to give some break?`,
-            `Are you still there? It's been hours ⏱️, keep up the good work. 👍`,
-            `You're unstoppable 🚀, keep it going. 👍`
-          ];
-          try {
-            await slackClientApp.client.chat.postMessage({
-              channel: item.user_id,
-              text: messages.random()
-            });
-          }
-          catch(e) {
-            res.log.error(e);
-          }
+        else {
+          res.log.error(mutation.error);
         }
-        else {}
       }
+      else if(durationInMinutes >= (process.env.PROLONGED_TIMER_TRESHOLD_2 || 420)) {
+        try {
+          await slackClientApp.client.chat.postMessage({
+            channel: item.user_id.slack_user_id,
+            text: `You're marvellous 👑, but I start to worry. Are you still there? 👀 I'll shut down your timer within the next hour.`
+          });
+        }
+        catch(e) {
+          res.log.error(e);
+        }
+      }
+      else if(durationInMinutes >= (process.env.PROLONGED_TIMER_TRESHOLD_1 || 240)) {
+        const messages = [
+          `You're such a hard worker 💪. It's been hours ⏱️ and you're still going. Keep it up. 👍`,
+          `It's been quite some time since you started 👀, care to give some break?`,
+          `Are you still there? It's been hours ⏱️, keep up the good work. 👍`,
+          `You're unstoppable 🚀, keep it going. 👍`
+          ];
+        try {
+          await slackClientApp.client.chat.postMessage({
+            channel: item.user_id.slack_user_id,
+            text: messages.random()
+          });
+        }
+        catch(e) {
+          res.log.error(e);
+        }
+      }
+      else {}
     });
     res.json({ok: true}); 
   }
   else {
     res.log.debug(req.body);
-    res.status(403).send({error: `Requested data is missing. Exiting.`});
+    res.status(404).send({error: `No running timers were found. Exiting.`});
   }
 }
+
+const TimersWithNoEndDateQuery = `
+  query Timers($startAt: String!) {
+    timers(filter: {ends_at: {_null: null}, starts_at: {_lte: $startAt}}) {
+      id
+      user_id {
+        email
+        slack_user_id
+      }
+      starts_at
+    }
+  }
+`;
 
 const StopTimerMutation = `
   mutation update_timers_item($timerId: ID!, $endsAt: Date!) {
