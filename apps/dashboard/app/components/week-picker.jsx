f7e9f10168b4254cfc6600a0325118581a4b8a82
Gokcen Ogutcu <gokcen@brewww.com>
2023-08-09 13:18:23 +0300

    fix: date span in schema for weekly stats

--- apps/dashboard/app/components/week-picker.jsx
+++ apps/dashboard/app/components/week-picker.jsx
@@ -7,7 +7,7 @@ const WeekPicker = ({ stats, timezone: zone, selectedDate }) => {
     DateTime.fromISO(selectedDate).startOf('month'),
     DateTime.fromISO(selectedDate).endOf('month')
   );
-  const weeks = monthlyInterval.splitBy(Duration.fromObject({ days: 6 }));
+  const weeks = monthlyInterval.splitBy(Duration.fromObject({ week: 1 }));
   return (
     <div className="self-stretch rounded-2xl bg-primary-light-baby-powder flex flex-row pt-6 px-6 pb-4 items-center justify-between text-center text-sm text-shades-of-teal-teal-400 font-primary-body-h4-p-medium">
       <Link to={`/dashboard/weekly/${DateTime.fromISO(selectedDate).minus({months: 1}).startOf('month').toISODate()}`} className="cursor-pointer [border:none] p-1 bg-[transparent] rounded-80xl flex flex-row items-center justify-center">
@@ -15,7 +15,7 @@ const WeekPicker = ({ stats, timezone: zone, selectedDate }) => {
       </Link>
       <div className="flex flex-row items-start justify-start gap-[20px]">
       {weeks.map(week => 
-        <Week key={week} date={week} duration={stats?.byInterval?.find(date => date.type == 'week' && DateTime.fromISO(date.startsAt).toISODate() == week.start.toISODate() && DateTime.fromISO(date.endsAt).toISODate() == week.end.toISODate())?.totalDuration}/>
+        <Week key={week} date={week} duration={stats?.byInterval?.find(date => date.type == 'week' && DateTime.fromISO(date.startsAt) >= week.start && DateTime.fromISO(date.endsAt) <= week.end)?.totalDuration}/>
       )}
       </div>
       <Link to={`/dashboard/weekly/${DateTime.fromISO(selectedDate).plus({months: 1}).startOf('month').toISODate()}`} className="cursor-pointer [border:none] p-1 bg-[transparent] rounded-80xl flex flex-row items-center justify-center">
--- apps/hooks/src/Routes/schema.js
+++ apps/hooks/src/Routes/schema.js
@@ -144,17 +144,17 @@ const schema = new GraphQLSchema({
           }));
           const byWeeklyIntervals = monthlyInterval.splitBy(Duration.fromObject({ week: 1 })).map(week => ({
             type: 'week',
-            totalDuration: timers.filter(timer => DateTime.fromISO(timer.starts_at) >= week.start && DateTime.fromISO(timer.ends_at) <= week.end).reduce((acc, timer) => acc + timer.total_duration, 0),
+            totalDuration: timers.filter(timer => DateTime.fromISO(timer.starts_at) >= week.start && DateTime.fromISO(timer.ends_at) <= week.end.endOf('day')).reduce((acc, timer) => acc + timer.total_duration, 0),
             startsAt: week.start.toISO(),
-            endsAt: week.end.toISO(),
+            endsAt: week.end.endOf('day').toISO(),
           }));
           return {
             byDate,
             byInterval: [
               {
                 type: 'month',
-                startsAt: DateTime.now().startOf('month').toISO(),
-                endsAt: DateTime.now().endOf('month').toISO(),
+                startsAt: DateTime.local({ zone: process.env.TIMEZONE || 'UTC' }).startOf('month').toISO(),
+                endsAt: DateTime.local({ zone: process.env.TIMEZONE || 'UTC' }).endOf('month').toISO(),
                 totalDuration: timers.reduce((acc, timer) => acc + timer.total_duration, 0)
               }, 
               ...byWeeklyIntervals,
