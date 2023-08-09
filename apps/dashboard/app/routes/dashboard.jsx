02253dd186f95677a82ae83ea9ad5dd46c6232b2
Gokcen Ogutcu <gokcen@brewww.com>
2023-08-09 12:00:37 +0300

    fix: full width

--- apps/dashboard/app/routes/dashboard.jsx
+++ apps/dashboard/app/routes/dashboard.jsx
@@ -23,8 +23,8 @@ export const loader = async ({ request }) => {
 export default function Dashboard() {
   const { email } = useLoaderData();
   return (
-    <div className="relative bg-shades-of-cadet-gray-cadet-gray-900 w-[1440px] h-[944px] left-[50%] transform translate-x-[-50%]">
-      <div className="absolute top-[0px] left-[0px] bg-primary-real-white w-[1440px] flex flex-col py-0 px-8 box-border items-center justify-start h-full">
+    <div className="relative bg-shades-of-cadet-gray-cadet-gray-900 w-full h-[944px] left-[50%] transform translate-x-[-50%]">
+      <div className="absolute top-[0px] left-[0px] bg-primary-real-white w-full flex flex-col py-0 px-8 box-border items-center justify-start h-full">
         <Header email={email} />
         <Outlet/>
       </div>
