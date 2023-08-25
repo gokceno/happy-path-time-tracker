c78221f1a15af2f5760cdc7ecf231ddcbc251f4c
Gokcen Ogutcu <gokcen@brewww.com>
2023-08-25 15:09:34 +0300

    chore

--- apps/dashboard/app/routes/dashboard.jsx
+++ apps/dashboard/app/routes/dashboard.jsx
@@ -29,6 +29,14 @@ export default function Dashboard() {
         <Header email={email} />
         <Outlet/>
       </div>
+      <Toast.Provider>
+        <Toast.Root open="true" className="bg-shades-of-teal-teal-50 rounded-md p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut">
+          <Toast.Description>
+            (TEST) Couldn't restart. You have a running timer. Please stop it first and try again.
+          </Toast.Description>
+        </Toast.Root>
+        <Toast.Viewport className="[--viewport-padding:_25px] fixed bottom-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none"/>
+      </Toast.Provider>
     </div>
   );
 }
