66f773e7ec45d1930b56a959fc6c44ea72df1bd4
Gokcen Ogutcu <gokcen@brewww.com>
2023-08-30 15:16:14 +0300

    fix: toast styling

--- apps/dashboard/app/routes/dashboard.jsx
+++ apps/dashboard/app/routes/dashboard.jsx
@@ -40,13 +40,13 @@ export default function Dashboard() {
         <Header email={email} />
         <Outlet/>
       </div>
-      <Toast.Provider duration={2000}>
+      <Toast.Provider duration={2500}>
         { notifications.map(notification =>  
           <Toast.Root 
             defaultOpen="true" 
             onOpenChange={(isOpen) => (setSearchParams({}))}
-            className="bg-shades-of-teal-teal-50 rounded-md p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut">
-            <Toast.Description>
+            className="bg-shades-of-teal-teal-400 rounded-md p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut">
+            <Toast.Description className="text-primary-real-white font-primary-small-body-h5-medium">
               { notification }
             </Toast.Description>
           </Toast.Root>
