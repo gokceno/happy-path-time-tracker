ab106d5c0054bc4f687df405d1592cb89aa4bc9e
Uğur Sözen <ugur@brewww.com>
2023-10-27 18:42:41 +0300

    feat: add alert dialog for logout action #2

--- apps/dashboard/app/components/avatar.jsx
+++ apps/dashboard/app/components/avatar.jsx
@@ -1,13 +1,12 @@
-const Avatar = ({ email }) => {
+const Avatar = ({ email, onClick }) => {
   return (
     <div className="relative w-10 h-10 flex flex-row items-center justify-center">
-      <a href="/logout">
-        <img
-          className="relative rounded-[50%] w-10 h-10 object-cover z-[0]"
-          alt={`User avatar for ${email}`}
-          src={`https://unavatar.io/${email}?fallback=https://source.boringavatars.com/beam/120/${email}`}
-        />
-      </a>
+      <img
+        className="relative rounded-[50%] w-10 h-10 object-cover z-[0] cursor-pointer"
+        alt={`User avatar for ${email}`}
+        src={`https://unavatar.io/${email}?fallback=https://source.boringavatars.com/beam/120/${email}`}
+        onClick={onClick}
+      />
     </div>
   );
 };
--- apps/dashboard/app/components/header.jsx
+++ apps/dashboard/app/components/header.jsx
@@ -1,14 +1,18 @@
-import PeriodNavigation from "./period-navigation";
 import Avatar from "./avatar";
+import PeriodNavigation from "./period-navigation";
 
-const Header = ({ email }) => {
+const Header = ({ email, onLogout }) => {
   return (
     <div className="self-stretch flex flex-row py-4 px-0 items-center justify-between">
       <a href="/">
-        <img className="relative w-[38px]" alt="Happy Path" src="/hummingbird.png" />
+        <img
+          className="relative w-[38px]"
+          alt="Happy Path"
+          src="/hummingbird.png"
+        />
       </a>
       <PeriodNavigation />
-      <Avatar email={email} />
+      <Avatar email={email} onClick={onLogout} />
     </div>
   );
 };
