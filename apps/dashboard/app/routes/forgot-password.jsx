e58cb62e801d3a86d046b05c173e8fabf49f414d
Gokcen Ogutcu <gokcen@brewww.com>
2024-06-29 15:53:26 +0300

    feat: added zod to forgot-password

--- apps/dashboard/app/routes/forgot-password.jsx
+++ apps/dashboard/app/routes/forgot-password.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react';
+import { useFetcher } from '@remix-run/react';
+import { json } from '@remix-run/node';
+import { Frontend as Client } from '@happy-path/graphql-client';
+import { z } from 'zod';
 
 export const meta = () => [{ title: 'Recover Account - Happy Path' }];
 
+const RecoverMutation = `
+  mutation PasswordResetRequest($email: String!) {
+    auth_password_request(email: $email)
+  }
+`;
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  try {
+    const email = z.string().email().parse(formData.get('email'));
+    const response = await Client({
+      url: (process.env.API_DIRECTUS_URL || '') + '/graphql/system',
+    }).mutation(RecoverMutation, {
+      email,
+    });
+    return json({ ok: !!response.data.auth_password_request });
+  } catch (e) {
+    return json({ ok: false });
+  }
+};
+
 const ForgotPassword = () => {
-  const [isInvalidAttempt, setIsInvalidAttempt] = useState(true);
-  const [isCodeSent, setIsCodeSent] = useState(true);
-
-  // make service call with email to send code
-  // if success, show success message with setIsCodeSent(true)
-  // if failure, show error message with setIsInvalidAttempt(true)
-
+  const fetcher = useFetcher();
   return (
     <div className="relative font-primary-small-body-h5-medium bg-shades-of-cadet-gray-cadet-gray-50 w-full min-h-screen flex items-center">
       <div className="flex flex-col max-w-[340px] w-full m-auto items-center justify-center">
@@ -23,7 +41,7 @@ const ForgotPassword = () => {
             Recover Account
           </h2>
 
-          {isCodeSent && (
+          {fetcher.data?.ok === true && (
             <div
               className="mt-4 -mb-4 text-sm bg-green-100 border text-center border-green-400 text-green-700 px-4 py-4 rounded-lg relative"
               role="alert"
@@ -31,24 +49,38 @@ const ForgotPassword = () => {
               <strong className="font-semibold">Horaay!</strong>
               <br />
               <span>
-                We have sent a code to your email. <br />
+                We sent a code to your email address. <br />
                 Please check your inbox.
               </span>
             </div>
           )}
 
+          {fetcher.data?.ok === false && (
+            <div
+              className="mt-4 -mb-4 text-sm bg-red-100 border text-center border-red-400 text-red-700 px-4 py-4 rounded-lg relative"
+              role="alert"
+            >
+              <strong className="font-semibold">Holy smokes!</strong>
+              <br />
+              <span>
+                An error occured, please try again. Contact the system administrator if the error
+                persists.
+              </span>
+            </div>
+          )}
+
           <div className="mt-8 w-full">
-            <form
+            <fetcher.Form
+              method="post"
+              action="/forgot-password"
               className="space-y-6"
-              action="#"
-              method="POST"
             >
               <div>
                 <label
                   htmlFor="email"
                   className="text-sm font-medium leading-6 text-gray-500"
                 >
-                  Email address
+                  Email Address
                 </label>
                 <div className="mt-2 w-full">
                   <input
@@ -63,13 +95,16 @@ const ForgotPassword = () => {
               </div>
 
               <div>
-                <button className="w-full cursor-pointer [border:none] mt-3 p-3 bg-shades-of-teal-teal-300 self-stretch rounded-9xl h-12 flex flex-row box-border items-center justify-center relative gap-[8px]">
+                <button
+                  type="submit"
+                  className="w-full cursor-pointer [border:none] mt-3 p-3 bg-shades-of-teal-teal-300 self-stretch rounded-9xl h-12 flex flex-row box-border items-center justify-center relative gap-[8px]"
+                >
                   <div className="relative text-base leading-[133%] font-medium font-primary-small-body-h5-semibold text-primary-real-white text-left z-[2]">
                     Send me the code
                   </div>
                 </button>
               </div>
-            </form>
+            </fetcher.Form>
 
             <div className="mt-6 text-center text-sm text-gray-500">
               <a
